c4e4e05604c5816848111c37c01086a1268f7515
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-04-30 18:30:38 +0530

    Add in-memory user authentication with custom UserDetailsService

--- src/main/java/com/springsecurity/configuration/SecurityConfig.java
+++ src/main/java/com/springsecurity/configuration/SecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,5 +29,19 @@ public class SecurityConfig {
 //        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("admin1")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
