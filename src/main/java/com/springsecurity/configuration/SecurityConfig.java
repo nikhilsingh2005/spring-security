409f422ff0b6654b6b1673d0bfd6c1a592fadafa
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-04-30 18:52:47 +0530

    Enable method-level security with @EnableMethodSecurity and @PreAuthorize

--- src/main/java/com/springsecurity/configuration/SecurityConfig.java
+++ src/main/java/com/springsecurity/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +19,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
--- src/main/java/com/springsecurity/controller/GreetingsController.java
+++ src/main/java/com/springsecurity/controller/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String greetings() {
         return "Hello World!";
+    }
+
+    @PreAuthorize( "hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin!";
+    }
+
+    @PreAuthorize( "hasRole('USER')")
+    @GetMapping("/user")
+    public String user() {
+        return "Hello User!";
     }
 }
