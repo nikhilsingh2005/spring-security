dfb6645e5137dda19f062b237110fbe27ffa91b4
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-04-30 12:40:42 +0530

    Initial commit

---
+++ src/main/java/com/springsecurity/controller/GreetingsController.java
@@ -0,0 +1,13 @@
+package com.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping
+    public String greetings() {
+        return "Hello World!";
+    }
+}
