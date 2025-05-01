d44e3fa4f9162c80455b8166d4f4b276467c832d
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-01 19:33:25 +0530

    Test BCrypt password encryption with JDBC-based authentication

--- src/main/java/com/springsecurity/configuration/SecurityConfig.java
+++ src/main/java/com/springsecurity/configuration/SecurityConfig.java
@@ -1,23 +1,26 @@
 package com.springsecurity.configuration;
 
-
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
@@ -43,16 +46,28 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.withUsername("user1")
-                .password("{noop}user")
-                .roles("USER")
-                .build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        UserDetails user2 = User.withUsername("admin1")
-                .password("{noop}admin")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2);
+    @Bean
+    public UserDetailsManager users(DataSource dataSource, PasswordEncoder encoder) {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+
+        if (!manager.userExists("user1")) {
+            manager.createUser(User.withUsername("user1")
+                    .password(encoder.encode("user"))
+                    .roles("USER")
+                    .build());
+        }
+
+        if (!manager.userExists("admin1")) {
+            manager.createUser(User.withUsername("admin1")
+                    .password(encoder.encode("admin"))
+                    .roles("ADMIN")
+                    .build());
+        }
+
+        return manager;
     }
 }
