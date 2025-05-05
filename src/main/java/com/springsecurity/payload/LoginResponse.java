6766797a6dbd813e59b82dffdbea85539cdf8be0
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-05 11:35:00 +0530

    add dtos for custom Login-Request and Login-Response

---
+++ src/main/java/com/springsecurity/payload/LoginResponse.java
@@ -0,0 +1,42 @@
+package com.springsecurity.payload;
+
+import java.util.List;
+
+public class LoginResponse {
+    private String jwt;
+    private String username;
+    private List<String> role;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public List<String> getRole() {
+        return role;
+    }
+
+    public void setRole(List<String> role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(List<String> role, String username, String jwt) {
+        this.role = role;
+        this.username = username;
+        this.jwt = jwt;
+    }
+}
